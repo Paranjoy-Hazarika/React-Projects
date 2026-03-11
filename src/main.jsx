747c32cdb6ba823e144a358b74fc076084bbba5b
Paranjoy <paranjoyhazarika@gmail.com>
2026-03-11 09:48:19 +0530

    Image issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './Dashboard.jsx'
 import Course from './assignments/Course/Course.jsx'
 import Todo from './assignments/Todo/Todo.jsx'
 
